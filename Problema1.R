1089b9f9e7590ce75d0e2add98c92c68adc18959
adria.sanchez.gaspar <adria.sanchez.gaspar@auleseebe.upc.edu>
2025-02-13 13:01:14 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,42 @@
 #Codigo para problema 1
 
-plot(1:5)
+mtcars
+dim(mtcars)
+
+mis_dades <- mtcars
+
+mis_dades
+
+#promedio / mitjana
+mean(mis_dades$qsec)
+
+x <- mis_dades$qsec
+x
+
+#calcular el promedio
+sum(x)/length(x)
+
+x <- mis_dades$drat
+x
+#ordenar per ordre de gran a petit 
+sort(x)
+
+#calcular la mediana
+median(x)
+
+#calcular los quartiles
+quantile(x)
+quantile(x,0.25)
+quantile(x,0.75)
+
+#calcular percentiles 
+quantile(mis_dades$mpg, 0.18)
+
+#calcular interquartil
+IQR(mis_dades$cyl)
+
+#calcular desviacion tipica (corregida) (se llama corregida porque esta dividida en n-1) 
+sd(mis_dades$cyl)
+
+#calcular la varianza (corregida)
+var(mis_dades$qsec)
